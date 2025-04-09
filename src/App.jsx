0c79d2671de47b77c242d34dae4ede0c9b71a1a2
AbhiSand90 <135419029+AbhiSand90@users.noreply.github.com>
2025-04-09 10:04:32 +0000

    yeen

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
  const App = ()=> {
   return( <div> 
     <h1>Anudip</h1>
-    <img src="src/images/image.png"></img>
+    <img src="public/images/image.png"></img>
 
     <p>We will taffic your children</p>
 
-    <img src="src/images/image copy.png"></img>
+    <img src="public/images/image copy.png"></img>
     
      </div> )
 }
